90dd189ec9ff9a72033cb8ee2395cad5b946472a
johflo <53262590+johflo@users.noreply.github.com>
2022-07-19 16:49:19 +0200

    Delete Seeed-LoRa-E5_PingPong directory

--- Seeed-LoRa-E5_PingPong/Debug/Utilities/misc/stm32_systime.d
+++
@@ -1,3 +0,0 @@
-Utilities/misc/stm32_systime.o: ../Utilities/misc/stm32_systime.c \
- ../Utilities/misc/stm32_systime.h
-../Utilities/misc/stm32_systime.h:
--- Seeed-LoRa-E5_PingPong/Debug/Utilities/misc/stm32_tiny_vsnprintf.d
+++
@@ -1,4 +0,0 @@
-Utilities/misc/stm32_tiny_vsnprintf.o: \
- ../Utilities/misc/stm32_tiny_vsnprintf.c \
- ../Utilities/misc/stm32_tiny_vsnprintf.h
-../Utilities/misc/stm32_tiny_vsnprintf.h:
