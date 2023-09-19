b49cd28604fd7a2be03be79b2625edb8ebf9afe3
johflo <florineth@gmail.com>
2023-09-19 22:26:14 +0200

    Update to FW 1.3.0

--- Seeed-LoRa-E5_PingPong/Debug/Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/lr_fhss_mac.d
+++
@@ -1,8 +0,0 @@
-Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/lr_fhss_mac.o: \
- ../Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/lr_fhss_mac.c \
- ../Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/lr_fhss_mac.h \
- ../Middlewares/Third_Party/SubGHz_Phy/radio_def.h \
- ../Middlewares/Third_Party/SubGHz_Phy/lr_fhss_v1_base_types.h
-../Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/lr_fhss_mac.h:
-../Middlewares/Third_Party/SubGHz_Phy/radio_def.h:
-../Middlewares/Third_Party/SubGHz_Phy/lr_fhss_v1_base_types.h:
--- Seeed-LoRa-E5_PingPong/Debug/Utilities/misc/stm32_tiny_sscanf.d
+++
@@ -1,3 +0,0 @@
-Utilities/misc/stm32_tiny_sscanf.o: ../Utilities/misc/stm32_tiny_sscanf.c \
- ../Utilities/misc/stm32_tiny_sscanf.h
-../Utilities/misc/stm32_tiny_sscanf.h:
